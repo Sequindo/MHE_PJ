f57ed7d3abd48c19263ab4f0318f753065e4b48e
Kacper Nowak <knowak@pop-os.localdomain>
2021-10-11 13:33:33 +0200

    Added validation and test case for independent set problem
    Signed-off-by: Kacper Nowak <knowak@pop-os.localdomain>

--- main.cpp
+++ main.cpp
@@ -7,12 +7,22 @@
 using graph_t = std::vector<std::vector<bool>>;
 using solution_t = std::vector<bool>; //vector with values (0,1) for each vertice - telling if given vertice belongs to independent set
 
+std::ostream& operator <<(std::ostream& os, const solution_t& sol)
+{
+    os << "Vertices with indexes: ";
+    for(int i=0;i<sol.size();i++)
+    {
+        if(sol.at(i)) os << i+1 << " ";
+    }
+    return os;
+}
+
 std::function<bool(solution_t&)> validation_function_factory(graph_t _graph)
 {
     return [=](solution_t& _vec_vector) -> bool {
         for(int i=0;i<_vec_vector.size();i++)
         {
-            if(_vec_vector.at(i)==false) continue; //given vertice does not belong to independent set
+            if(_vec_vector.at(i)==false) continue;
             for(int j=0;j<_vec_vector.size();j++)
             {
                 if(i==j || _vec_vector[j]==false) continue;
@@ -21,11 +31,11 @@ std::function<bool(solution_t&)> validation_function_factory(graph_t _graph)
                     if(_graph.at(i).at(j)==true || _graph.at(j).at(i)==true)
                     {
                         std::string s;
-                        s.append("Podane wierzcholki ");
+                        s.append("Given vertices ");
                         s.append(std::to_string(i));
                         s.append(" i ");
                         s.append(std::to_string(j));
-                        s.append(" sa incydentne!\n");
+                        s.append(" are adjacent!\n");
                         throw std::string(s);
                     }
                 }
@@ -47,11 +57,19 @@ void test_if_it_works()
     };
 
     auto validation_func = validation_function_factory(test_graph);
-    solution_t s1_candidate = {1, 0, 1, 0, 0, 1};
-    solution_t s2_candidate = {1, 0, 1, 0, 1, 1};
-    if(validation_func(s1_candidate))
+    std::vector<solution_t> solutions;
+    solutions.push_back(solution_t{1, 0, 1, 0, 0, 1});
+    solutions.push_back(solution_t{1, 0, 1, 0, 1, 1});
+    for(auto sol : solutions)
     {
-        std::cout << "S1 opisuje zbior niezalezny" << std::endl;
+        try
+        {
+            bool ret = validation_func(sol);
+            if(ret) std::cout << sol <<  "describes an independent set\n";
+        } catch(std::string &str)
+        {
+            std::cout << str << std::endl;
+        };
     }
 }
 
