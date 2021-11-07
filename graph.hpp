7965e95bbe275cf1a3836b9345f578ef2fe2ea6d
Kacper Nowak <knowak@pop-os.localdomain>
2021-11-07 01:36:59 +0100

    Brute force method implementation + code refactor 1/n
    Signed-off-by: Kacper Nowak <knowak@pop-os.localdomain>

--- graph.hpp
+++ graph.hpp
@@ -1,19 +1,21 @@
-#pragma once
 #include <algorithm>
 #include <vector>
 #include <fstream>
 #include <functional>
 #include <random>
+#include <chrono>
 
 using graph_desc_t = std::vector<std::vector<bool>>;
 using solution_t = std::vector<bool>; //vector with values (0,1) for each vertice - telling if given vertice belongs to independent set
 class Graph {
-    graph_desc_t _desc_graph;
+    graph_desc_t desc_graph;
 
 public:
     Graph() {}
-    Graph(Graph& _other) {}
-    Graph(Graph&& other) {}
+    Graph(Graph& _other) : desc_graph(_other.desc_graph) {}
+    Graph(Graph&& _other) : desc_graph(std::move(_other.desc_graph)) {}
+    Graph(graph_desc_t& _desc_graph) : desc_graph(_desc_graph) {}
+    Graph(graph_desc_t&& _desc_graph) : desc_graph(std::move(_desc_graph)) {}
 
     void readFromFile(const char* filename)
     {
@@ -39,11 +41,118 @@ public:
                         break;
                 }
             }
-            _desc_graph.push_back(tmp);
+            desc_graph.push_back(tmp);
         }
         filestream.close();
         }
-    void prepareGraphVizOutput(const char* filename, const solution_t& sol={})
+
+    std::function<int(solution_t&)> validation_function_factory()
+    {
+        return [=](solution_t& _vec_vector) -> int {
+            int size_set = std::count(_vec_vector.begin(), _vec_vector.end(), true); //proposed set size
+            for(size_t i=0;i<_vec_vector.size();i++)
+            {
+                if(_vec_vector.at(i)==false) continue;
+                for(size_t j=0;j<_vec_vector.size();j++)
+                {
+                    if(i==j || _vec_vector[j]==false) continue;
+                    else
+                    {
+                        if(desc_graph.at(i).at(j)==true || desc_graph.at(j).at(i)==true)
+                        {
+                            std::string s;
+                            s.append("Given vertices ");
+                            s.append(std::to_string(i));
+                            s.append(" i ");
+                            s.append(std::to_string(j));
+                            s.append(" are adjacent!\n");
+                            throw std::string(s);
+                        }
+                    }
+                }
+            }
+            return size_set;
+        };
+    }
+    size_t getGraphSize()
+    {
+        return desc_graph.size();
+    }
+    graph_desc_t getGraphDesc()
+    {
+        return desc_graph;
+    }
+
+    static solution_t generateSolution(const size_t& size)
+    {
+        std::random_device rd;
+        std::mt19937 generator(rd());
+        std::uniform_int_distribution<int> distribution(0,1);
+        auto roll = std::bind (distribution, generator);
+
+        solution_t _solution(size,false);
+        for(auto&& v : _solution)
+        {
+            v = roll();
+        }
+        return _solution;
+    }
+
+    static solution_t nextSolution(const solution_t& _solution) //increment vector by a logical 1
+    {
+        solution_t _newSolution = _solution;
+        for(auto it = _newSolution.rbegin(); it != _newSolution.rend(); it++)
+        {
+            *it = !*it;
+            if(*it) break;
+        }
+        return _newSolution;
+    }
+
+    static graph_desc_t generateProblemGraph(size_t size, const char* filename=NULL)
+    {
+        std::random_device rd;
+        std::mt19937 generator(rd());
+        std::uniform_int_distribution<int> distribution(0,1);
+        auto roll = std::bind (distribution, generator);
+
+        graph_desc_t newGraph;
+        solution_t tmp;
+        solution_t sol = std::move(generateSolution(size));
+        sol[0] = false;
+        newGraph.push_back(sol);
+        for(int i=1;i<size;i++) 
+        {
+            tmp.clear();
+            for(int j=0;j<i;j++)
+            {
+                tmp.push_back(newGraph.at(j).at(i));
+            }
+            tmp.push_back(false);
+            solution_t tmp_rand = std::move(generateSolution(size-i-1));
+            tmp.insert(tmp.end(), tmp_rand.begin(), tmp_rand.end());
+            newGraph.push_back(tmp);
+        }
+        if(filename!=NULL)
+        {
+            std::ofstream ofs;
+            ofs.open(filename);
+            for(auto& sol_row : newGraph)
+            {
+                for(int i=0;i<sol_row.size();i++)
+                {
+                    ofs << sol_row.at(i);
+                    i+1 == sol_row.size() ? ofs << ";" : ofs << ",";
+                }
+                ofs << std::endl;
+            }
+            ofs.close();
+        }
+        return newGraph;
+    }
+
+    static void prepareGraphVizOutput(const graph_desc_t& graph_desc, const char* filename, const solution_t& sol={}, 
+        const std::chrono::duration<double> timestamp={})
     {
         std::ofstream ofs(filename, std::ios::trunc);
         if(!ofs.is_open())
@@ -51,12 +160,13 @@ public:
             throw std::string("Cannot open file for writing!\n");
         }
         ofs << "strict graph G {\n\tnode [shape=circle style=filled]\n";
-        int v_num = _desc_graph.at(0).size();
+        ofs << "\tlabel=\"Elapsed calculating time: " << timestamp.count() << " s\"\n";
+        int v_num = graph_desc.at(0).size();
         for(int i=0;i<v_num; i++)
         {
             for(int j=0;j<v_num;j++)
             {
-                if(_desc_graph.at(i).at(j))
+                if(graph_desc.at(i).at(j))
                 {
                     ofs << "\t" << i+1 << "--" << j+1 << "\n";
                 }
@@ -73,58 +183,5 @@ public:
         }
         ofs << "}";
         ofs.close();
-    }
-    std::function<int(solution_t&)> validation_function_factory()
-    {
-        return [=](solution_t& _vec_vector) -> int {
-            int size_set = std::count(_vec_vector.begin(), _vec_vector.end(), true); //proposed set size
-            for(size_t i=0;i<_vec_vector.size();i++)
-            {
-                if(_vec_vector.at(i)==false) continue;
-                for(size_t j=0;j<_vec_vector.size();j++)
-                {
-                    if(i==j || _vec_vector[j]==false) continue;
-                    else
-                    {
-                        if(_desc_graph.at(i).at(j)==true || _desc_graph.at(j).at(i)==true)
-                        {
-                            std::string s;
-                            s.append("Given vertices ");
-                            s.append(std::to_string(i));
-                            s.append(" i ");
-                            s.append(std::to_string(j));
-                            s.append(" are adjacent!\n");
-                            throw std::string(s);
-                        }
-                    }
-                }
-            }
-            return size_set;
-        };
-    }
-
-    solution_t generateSolution()
-    {
-        std::random_device rd;
-        std::mt19937 generator(rd());
-        std::uniform_int_distribution<int> distribution(0,1);
-        auto roll = std::bind (distribution, generator);
-
-        solution_t _solution(_desc_graph.size(),false);
-        for(auto&& v : _solution)
-        {
-            v = roll();
-        }
-        return _solution;
-    }
-    solution_t nextSolution(const solution_t& _solution) //increment vector by a logical 1
-    {
-        solution_t _newSolution = _solution;
-        for(auto it = _newSolution.rbegin(); it != _newSolution.rend(); it++)
-        {
-            *it = !*it;
-            if(*it) break;
-        }
-        return _newSolution;
     }
 };
--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-
 #include "graph.hpp"
 
+#define SHORT_PROBLEM_SIZE 10
 
 std::ostream& operator <<(std::ostream& os, const solution_t& sol)
 {
@@ -13,35 +13,44 @@ std::ostream& operator <<(std::ostream& os, const solution_t& sol)
     return os;
 }
 
+template<typename DurationType = std::chrono::seconds>
+void bruteForceMethod(Graph& graph, const char* graphvizFilenameOutput)
+{
+    uint8_t set_size = 0;
+    solution_t pInitialSolution, pSolutionNext;
+    pInitialSolution = std::move(Graph::generateSolution(graph.getGraphSize()));
+    pSolutionNext = pInitialSolution;
+    auto start = std::chrono::high_resolution_clock::now();
+    do
+    {
+        try
+        {
+            pSolutionNext = std::move(graph.nextSolution(pSolutionNext));
+            auto validation_func = graph.validation_function_factory();
+            int curr_set_size = validation_func(pSolutionNext);
+            if(curr_set_size > set_size)
+            {
+                set_size = curr_set_size;
+            }
+        }
+        catch(std::string &e)
+        {
+            //std::cerr << e << std::endl;
+        }
+    } while(pSolutionNext != pInitialSolution);
+    auto end = std::chrono::high_resolution_clock::now();
+    auto time_span = std::chrono::duration_cast<DurationType>(end - start);
+    Graph::prepareGraphVizOutput(graph.getGraphDesc(), graphvizFilenameOutput, pSolutionNext, time_span);
+    return;
+}
+
 int main(int argc, char **argv)
 {
-    Graph problemGraph;
-    int set_size = 0;
-
+    Graph problemGraphShort{std::move(Graph::generateProblemGraph(SHORT_PROBLEM_SIZE, "generated_graph.csv"))};
+    bruteForceMethod<std::chrono::milliseconds>(problemGraphShort, "graphviz_output_short.txt");
     if(argc>1)
     {
-        problemGraph.readFromFile(argv[1]);
-        solution_t pInitialSolution, pSolutionNext;
-        pInitialSolution = std::move(problemGraph.generateSolution());
-        pSolutionNext = pInitialSolution;
-        do
-        {
-            try
-            {
-                pSolutionNext = std::move(problemGraph.nextSolution(pSolutionNext));
-                auto validation_func = problemGraph.validation_function_factory();
-                int curr_set_size = validation_func(pSolutionNext);
-                if(curr_set_size > set_size)
-                {
-                    set_size = curr_set_size;
-                    problemGraph.prepareGraphVizOutput("graphviz.txt", pSolutionNext);
-                }
-            }
-            catch(std::string &e)
-            {
-                std::cerr << e << std::endl;
-            }
-        } while(pSolutionNext != pInitialSolution);
+        //problemGraph.readFromFile(argv[1]);
     }
     return 0;
 }
