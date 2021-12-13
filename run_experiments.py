de70808abb8f82c44750593ab445ef5feb20d821
Kacper Nowak <kacpernowak@pop-os.localdomain>
2021-12-13 18:28:56 +0100

    Add plotting data in Python script for stochastic hill climb
    Signed-off-by: Kacper Nowak <kacpernowak@pop-os.localdomain>

--- run_experiments.py
+++ run_experiments.py
@@ -3,6 +3,7 @@ import re;
 
 import subprocess;
 import numpy as np;
+import matplotlib.pyplot as plt
 
 statistics = {
     "-hclimb":[]
@@ -10,10 +11,10 @@ statistics = {
 
 sizes = {
     "-size_small",
-    #"-size_middle",
-    #"-size_large",
-    #"-size_largest",
-    #"-size_extralarge"
+    "-size_middle",
+    "-size_large",
+    "-size_largest",
+    "-size_extralarge"
 }
 
 path = "/home/kacpernowak/Repos/MHE_PJ/build/"
@@ -26,21 +27,29 @@ path = "/home/kacpernowak/Repos/MHE_PJ/build/"
 #run hill climb method for graphs of each size specified
 for method_name in statistics:
     for size in sizes:
-        for iteration in range(1,20):
-            per_size_time = []
-            per_size_quality = []
+        per_iteration_time = []
+        per_iteration_quality = []
+        for iteration in range(1,25):
             for repeat in range(1,25):
+                per_repeat_time = []
+                per_repeat_quality = []
                 cmndName = str(path)+"/MHE_PJ "+" -print_result "+str(method_name)+" -gfile \""+str(path)+"/generated_graph"+str(size).replace("-size","")+".csv\""+" -iter "+str(iteration)+" -ofile \"hclimb_output"+str(size).replace("-size","")+".txt\""
-                #print(cmndName)
                 result = os.popen(cmndName)
                 output = result.read().splitlines()
                 st = float(output[0])
                 sq = float(output[1])
-                per_size_time.append(st)
-                per_size_quality.append(sq)
-            mean_time_per_iter_size = np.mean(per_size_time)
-            mean_quality_per_iter_size = np.mean(per_size_quality)
-            statistics[method_name].append([size, iteration, mean_time_per_iter_size, mean_quality_per_iter_size])
+                per_repeat_time.append(st)
+                per_repeat_quality.append(sq)
+            per_iteration_time.append(np.mean(per_repeat_time))
+            per_iteration_quality.append(np.mean(per_repeat_quality))
+        print(per_iteration_time)
+        print(per_iteration_quality)
+        plt.plot(range(1,25), per_iteration_time)
+        plt.xlabel('Liczba iteracji')
+        plt.ylabel('Średni czas algorytmu')
+        plt.show()
+        plt.plot(range(1,25), per_iteration_quality)
+        plt.xlabel('Liczba iteracji')
+        plt.ylabel('Ilość wyznaczonych wierzchołków')
+        plt.show()
 
-for method_name in statistics:
-    print(statistics[method_name])
