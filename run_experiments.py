ec7358b0ff107f71c6c7620f2955e5e93b20b0c7
Kacper Nowak <kacpernowak@pop-os.localdomain>
2021-12-13 18:03:44 +0100

    Add Python script for running experiments - add experiments for stochastic hill climb
    Signed-off-by: Kacper Nowak <kacpernowak@pop-os.localdomain>

--- run_experiments.py
+++ run_experiments.py
@@ -1,7 +1,8 @@
 import os;
 import re;
-import numpy as np;
+
 import subprocess;
+import numpy as np;
 
 statistics = {
     "-hclimb":[]
@@ -9,12 +10,37 @@ statistics = {
 
 sizes = {
     "-size_small",
-    "-size_middle",
-    "-size_large",
-    "-size_largest",
-    "-size_extralarge"
+    #"-size_middle",
+    #"-size_large",
+    #"-size_largest",
+    #"-size_extralarge"
 }
 
-for size in sizes: #generate graph for each given size
-    cmndName = "./MHE_PJ " + str(size) +  " -gen_only -gfile \"generated_graph" + str(size).replace("-size","") + ".csv\""
-    os.popen(cmndName)
+path = "/home/kacpernowak/Repos/MHE_PJ/build/"
+
+#generate graph for each given size
+#for size in sizes:
+#    cmndName = str(path)+"MHE_PJ " + str(size) +  " -gen_only -gfile \""+str(path)+"generated_graph" + str(size).replace("-size","") + ".csv\""
+#    os.popen(cmndName)
+
+#run hill climb method for graphs of each size specified
+for method_name in statistics:
+    for size in sizes:
+        for iteration in range(1,20):
+            per_size_time = []
+            per_size_quality = []
+            for repeat in range(1,25):
+                cmndName = str(path)+"/MHE_PJ "+" -print_result "+str(method_name)+" -gfile \""+str(path)+"/generated_graph"+str(size).replace("-size","")+".csv\""+" -iter "+str(iteration)+" -ofile \"hclimb_output"+str(size).replace("-size","")+".txt\""
+                #print(cmndName)
+                result = os.popen(cmndName)
+                output = result.read().splitlines()
+                st = float(output[0])
+                sq = float(output[1])
+                per_size_time.append(st)
+                per_size_quality.append(sq)
+            mean_time_per_iter_size = np.mean(per_size_time)
+            mean_quality_per_iter_size = np.mean(per_size_quality)
+            statistics[method_name].append([size, iteration, mean_time_per_iter_size, mean_quality_per_iter_size])
+
+for method_name in statistics:
+    print(statistics[method_name])
